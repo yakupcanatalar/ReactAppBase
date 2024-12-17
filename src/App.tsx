31e0cc4cb489f72936ff4bee6aaf860c61a725ad
CagriAtalar <cagri.atalar@hotmail.com>
2024-12-17 22:02:44 +0300

    Removed unused code blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 
 const App = () => {
-  const navigate = useNavigate();
-
-  const goToRegister = () => {
-    navigate("/register"); // Register sayfasına yönlendir
-  };
 
   return (
     <div className="App">
