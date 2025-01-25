8b84c33a6cf22b6764c988ba1a13f5b8def4e916
Yakup can Atalar <atalaryakupcan@gmail.com>
2025-01-25 20:55:18 +0300

    Added left-sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import LeftSideBar from "./components/Left-Sidebar/Left-Sidebar";
 import Login from "./components/Login";
 
 const App = () => {
 
   return (
-    <div className="App">
-      <Login />
+    // <div className="App">
+    //   <Login />
+    // </div>
+
+
+    <div className="d-flex">
+      <LeftSideBar />
+      <div className="flex-grow-1 p-3">
+      </div>
     </div>
   );
 };
